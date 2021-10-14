6de29e1b363a48fac6d2ff17cb338631edb85d09
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2021-10-14 19:42:10 -0300

    Feita refatoracao de codigo no JS.

--- js/index.js
+++ js/index.js
@@ -2,158 +2,60 @@ function makeRequest(urlParam) {
     return fetch(`https://swapi.dev/api/${urlParam}`)
 }
 
+function createContent(res, ulID, aHref) {
+    // Pegar minha <ul> no HTML pelo ID
+    const ul = document.getElementById(ulID);
+
+    for (let x = 0; x < res.results.length; x++) {
+        // Criar uma <li>
+        const li = document.createElement("li");
+
+        // Criar <a>
+        const a = document.createElement("a");
+
+        // Colocar HREF no <a>
+        a.setAttribute("href", aHref);
+
+        ulID === "list-films" ? a.innerHTML = res.results[x].title : a.innerHTML = res.results[x].name;
+
+        li.appendChild(a);
+
+        ul.appendChild(li);
+    }
+}
+
 function getPeople() {
     makeRequest("people")
         .then(res => res.json())
-        .then(res => {
-
-            // Pegar minha <ul> no HTML pelo ID
-            const ul = document.getElementById("list-people");
-
-            for (let x = 0; x < res.results.length; x++) {
-                // Criar uma <li>
-                const li = document.createElement("li");
-
-                // Criar <a>
-                const a = document.createElement("a");
-
-                // Colocar HREF no <a>
-                a.setAttribute("href", "./person.html");
-                a.innerHTML = res.results[x].name;
-
-                li.appendChild(a);
-
-                ul.appendChild(li);
-            }
-        })
+        .then(res => createContent(res, "list-people", "./person.html"))
 }
 
 function getPlanets() {
     makeRequest("planets")
         .then(res => res.json())
-        .then(res => {
-
-            // Pegar minha <ul> no HTML pelo ID
-            const ul = document.getElementById("list-planets");
-
-            for (let x = 0; x < res.results.length; x++) {
-                // Criar uma <li>
-                const li = document.createElement("li");
-
-                // Criar <a>
-                const a = document.createElement("a");
-
-                // Colocar HREF no <a>
-                a.setAttribute("href", "./planet.html");
-                a.innerHTML = res.results[x].name;
-
-                li.appendChild(a);
-
-                ul.appendChild(li);
-            }
-        })
+        .then(res => createContent(res, "list-planets", "./planet.html"))
 }
 
 function getFilms() {
     makeRequest("films")
         .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            // Pegar minha <ul> no HTML pelo ID
-            const ul = document.getElementById("list-films");
-
-            for (let x = 0; x < res.results.length; x++) {
-                // Criar uma <li>
-                const li = document.createElement("li");
-
-                // Criar <a>
-                const a = document.createElement("a");
-
-                // Colocar HREF no <a>
-                a.setAttribute("href", "./film.html");
-                a.innerHTML = res.results[x].title;
-
-                li.appendChild(a);
-
-                ul.appendChild(li);
-            }
-        });
+        .then(res => createContent(res, "list-films", "./film.html"));
 }
 
 function getSpecies() {
     makeRequest("species")
         .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            // Pegar minha <ul> no HTML pelo ID
-            const ul = document.getElementById("list-species");
-
-            for (let x = 0; x < res.results.length; x++) {
-                // Criar uma <li>
-                const li = document.createElement("li");
-
-                // Criar <a>
-                const a = document.createElement("a");
-
-                // Colocar HREF no <a>
-                a.setAttribute("href", "./specie.html");
-                a.innerHTML = res.results[x].name;
-
-                li.appendChild(a);
-
-                ul.appendChild(li);
-            }
-        });
+        .then(res => createContent(res, "list-species", "./specie.html"));
 }
 
 function getVehicles() {
     makeRequest("vehicles")
         .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            // Pegar minha <ul> no HTML pelo ID
-            const ul = document.getElementById("list-vehicles");
-
-            for (let x = 0; x < res.results.length; x++) {
-                // Criar uma <li>
-                const li = document.createElement("li");
-
-                // Criar <a>
-                const a = document.createElement("a");
-
-                // Colocar HREF no <a>
-                a.setAttribute("href", "./vehicle.html");
-                a.innerHTML = res.results[x].name;
-
-                li.appendChild(a);
-
-                ul.appendChild(li);
-            }
-        });
+        .then(res => createContent(res, "list-vehicles", "./vehicle.html"));
 }
 
 function getStarships() {
     makeRequest("starships")
         .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            // Pegar minha <ul> no HTML pelo ID
-            const ul = document.getElementById("list-starships");
-
-            for (let x = 0; x < res.results.length; x++) {
-                // Criar uma <li>
-                const li = document.createElement("li");
-
-                // Criar <a>
-                const a = document.createElement("a");
-
-                // Colocar HREF no <a>
-                a.setAttribute("href", "./starship.html");
-                a.innerHTML = res.results[x].name;
-
-                li.appendChild(a);
-
-                ul.appendChild(li);
-            }
-        });
+        .then(res => createContent(res, "list-starships", "./starship.html"));
 }
