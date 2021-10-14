fee31cbd01bbc08daa15f5b73d04380fa05d6dc8
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2021-10-14 19:19:46 -0300

    Criada logica de busca de planetas e refatorado o fetch para ser usado em todas as requisicoes.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+function makeRequest(urlParam) {
+    return fetch(`https://swapi.dev/api/${urlParam}`)
+}
+
 function getPeople() {
-    fetch("https://swapi.dev/api/people/")
+    makeRequest("people")
         .then(res => res.json())
         .then(res => {
 
@@ -17,6 +21,34 @@ function getPeople() {
 
                 // Colocar HREF no <a>
                 a.setAttribute("href", "./person.html");
+                a.innerHTML = res.results[x].name;
+
+                li.appendChild(a);
+
+                ul.appendChild(li);
+            }
+        })
+}
+
+function getPlanets() {
+    makeRequest("planets")
+        .then(res => res.json())
+        .then(res => {
+
+            console.log(res);
+
+            // Pegar minha <ul> no HTML pelo ID
+            const ul = document.getElementById("list-planets");
+
+            for (let x = 0; x < res.results.length; x++) {
+                // Criar uma <li>
+                const li = document.createElement("li");
+
+                // Criar <a>
+                const a = document.createElement("a");
+
+                // Colocar HREF no <a>
+                a.setAttribute("href", "./planet.html");
                 a.innerHTML = res.results[x].name;
 
                 li.appendChild(a);
